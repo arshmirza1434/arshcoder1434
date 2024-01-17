c692da91edcbd76691ddc609583104e74dbd9d4b
arshmirza1434 <125905703+arshmirza1434@users.noreply.github.com>
2024-01-17 17:18:34 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -291,17 +291,19 @@ document.onkeydown = function (e) {
     }
 }
 
-// Start of Tawk.to Live Chat
-var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-(function () {
-    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-    s1.async = true;
-    s1.src = 'https://embed.tawk.to/65a7a60b0ff6374032c14811/1hkbe17ui';
-    s1.charset = 'UTF-8';
-    s1.setAttribute('crossorigin', '*');
-    s0.parentNode.insertBefore(s1, s0);
+// <!--Start of Tawk.to Script-->
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/65a7a60b0ff6374032c14811/1hkbe17ui';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
 })();
-// End of Tawk.to Live Chat
+
+// <!--End of Tawk.to Script-->
 
 
 /* ===== SCROLL REVEAL ANIMATION ===== */
